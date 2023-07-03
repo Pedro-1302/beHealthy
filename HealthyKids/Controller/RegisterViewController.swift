49c0b271359d3c3e9046df859a918a945ede2a0c
Gabriel S. Rossi <gabrieldesouzarossi123456@gmail.com>
2023-07-03 08:35:51 -0300

    update rossi new branch

---
+++ HealthyKids/Controller/RegisterViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RegisterViewController.swift
+//  HealthyKids
+//
+//  Created by Gabriel Rossi on 03/07/23.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    @IBOutlet var registerViewController: UIView!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}

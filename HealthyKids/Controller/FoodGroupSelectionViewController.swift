b24228e8960f699d28e178421bfa66f164dce481
Cassiano Salgado <103959564+CassianoSalgado@users.noreply.github.com>
2023-07-03 08:35:17 -0300

    Add files for the group selection screen

---
+++ HealthyKids/Controller/FoodGroupSelectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FoodGroupSelectionViewController.swift
+//  HealthyKids
+//
+//  Created by Cassiano C Salgado on 03/07/23.
+//
+
+import UIKit
+
+class FoodGroupSelectionViewController: UIViewController {
+
+    @IBOutlet weak var foodGroupsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
